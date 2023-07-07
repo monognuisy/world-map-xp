770d36a8c4d12793ebf9bdb1844b8eee2c19b133
monognuisy <ysmsmart1@gmail.com>
2023-07-07 09:34:04 +0900

    test: polygon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
-mapboxgl.accessToken = 'pk.eyJ1IjoibW9ub2dudWlzeSIsImEiOiJjbGpyM3NtNW4wY2JzM2JuZ2U0NGl6N2xkIn0._f_HHkotdV3pQUahw0kUBg';
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibW9ub2dudWlzeSIsImEiOiJjbGpyM3NtNW4wY2JzM2JuZ2U0NGl6N2xkIn0._f_HHkotdV3pQUahw0kUBg';
 
 export default function App() {
   const mapContainer = useRef(null);
@@ -12,13 +13,61 @@ export default function App() {
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
+
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [lng, lat],
-      zoom: zoom
+      zoom: zoom,
     });
+
+    const currMap = map?.current;
+  
+    currMap.on('load', () => {
+      // Add a source for the state polygons.
+      currMap.addSource('states', {
+        type: 'geojson',
+        data: 'https://docs.mapbox.com/mapbox-gl-js/assets/ne_110m_admin_1_states_provinces_shp.geojson',
+      });
+  
+      // Add a layer showing the state polygons.
+      currMap.addLayer({
+        id: 'states-layer',
+        type: 'fill',
+        source: 'states',
+        paint: {
+          'fill-color': 'rgba(200, 100, 240, 0.4)',
+          'fill-outline-color': 'rgba(200, 100, 240, 1)',
+        },
+      });
+  
+      // When a click event occurs on a feature in the states layer,
+      // open a popup at the location of the click, with description
+      // HTML from the click event's properties.
+      currMap.on('click', 'states-layer', (e) => {
+        new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML(e.features[0].properties.name)
+          .addTo(currMap);
+      });
+  
+      // Change the cursor to a pointer when
+      // the mouse is over the states layer.
+      currMap.on('mouseenter', 'states-layer', () => {
+        currMap.getCanvas().style.cursor = 'pointer';
+      });
+  
+      // Change the cursor back to a pointer
+      // when it leaves the states layer.
+      currMap.on('mouseleave', 'states-layer', () => {
+        currMap.getCanvas().style.cursor = '';
+      });
+    });
+    
+    console.log(currMap);
   });
+
+  
 
   return (
     <div>
