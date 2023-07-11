1c833360877398b2907af196a7fbe23f306677aa
monognuisy <ysmsmart1@gmail.com>
2023-07-11 18:28:44 +0900

    feat: click시 color 변화

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,4 +1,11 @@
 export const polyColors = {
-  transparent: 'rgba(0, 0, 0, 0)',
-  purple: 'rgb(220, 80, 255)'
-}
+  transparent: 'rgba(0, 0, 0, 0.1)',
+
+  lightBlueTrans: 'rgba(88, 220, 237, 0.5)',
+  lightBlue: 'rgba(88, 220, 237, 0.7)',
+
+  green: 'rgba(140, 237, 88, 0.7)',
+  yellow: 'rgba(237, 210, 88, 0.7)',
+  red: 'rgba(237, 110, 88, 0.7)',
+  purple: 'rgba(202, 88, 237, 0.7)',
+};
